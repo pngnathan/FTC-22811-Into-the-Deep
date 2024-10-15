8511f12eee338e12af035d3a26196067732fed5b
Nathan Morris <nmorris@alberton.k12.mt.us>
2024-10-15 17:30:08 -0600

    Got code working on the control hub for the first time. Copied in RobotHardware class to handle all hardware implementations. New opModes should reference RobotHardware rather than defining hardwareMap objects themselves to keep all hardware changes in one place.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MorrisPOVDrive.java
@@ -0,0 +1,202 @@
+package org.firstinspires.ftc.teamcode;
+/* Copyright (c) 2022 FIRST. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted (subject to the limitations in the disclaimer below) provided that
+ * the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this list
+ * of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice, this
+ * list of conditions and the following disclaimer in the documentation and/or
+ * other materials provided with the distribution.
+ *
+ * Neither the name of FIRST nor the names of its contributors may be used to endorse or
+ * promote products derived from this software without specific prior written permission.
+ *
+ * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
+ * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.util.Range;
+
+
+/*
+ * This OpMode illustrates how to use an external "hardware" class to modularize all the robot's sensors and actuators.
+ * This approach is very efficient because the same hardware class can be used by all of your teleop and autonomous OpModes
+ * without requiring many copy & paste operations.  Once you have defined and tested the hardware class with one OpMode,
+ * it is instantly available to other OpModes.
+ *
+ * The real benefit of this approach is that as you tweak your robot hardware, you only need to make changes in ONE place (the Hardware Class).
+ * So, to be effective you should put as much or your hardware setup and access code as possible in the hardware class.
+ * Essentially anything you do with hardware in BOTH Teleop and Auto should likely go in the hardware class.
+ *
+ * The Hardware Class is created in a separate file, and then an "instance" of this class is created in each OpMode.
+ * In order for the class to do typical OpMode things (like send telemetry data) it must be passed a reference to the
+ * OpMode object when it's created, so it can access all core OpMode functions.  This is illustrated below.
+ *
+ * In this concept sample, the hardware class file is called org.firstinspires.ftc.teamcode.RobotHardware.java and it must accompany this sample OpMode.
+ * So, if you copy org.firstinspires.ftc.teamcode.MorrisPOVDrive.java into TeamCode (using Android Studio or OnBotJava) then org.firstinspires.ftc.teamcode.RobotHardware.java
+ * must also be copied to the same location (maintaining its name).
+ */
+
+/**
+ * Mr. Morris:          TO DO:  1) Consider remapping buttons for movement to one joystick and arm rotation to other joystick,
+ *                                 or triggers for forward/backward, left stick for turning, right stick for steering
+ *                              2) Set preset locations for arm height and maybe gripper limits
+ *
+ *     COMPLETE, NEEDS TESTING: 1) Finish updating to match RobotHardware file definitions, then delete or comment out @Disabled
+ *                              2) Enable Camera live view
+ *                              3) Use math to keep wrist turned so that gripper is level with ground (rotate relative to arm rotation)
+ *                              4) Add elapsed time tracking and implement better button press delay method. See https://stemrobotics.cs.pdx.edu/node/7262.html
+ */
+
+@TeleOp(name="Morris POV Drive", group="Robot")
+//@Disabled
+public class MorrisPOVDrive extends LinearOpMode {
+
+    // Create a org.firstinspires.ftc.teamcode.RobotHardware object to be used to access robot hardware.
+    // Prefix any hardware functions with "robot." to access this class.
+    RobotHardware robot       = new RobotHardware(this);
+
+    @Override
+    public void runOpMode() {
+        double drive;
+        double turn;
+//        double armRotateTarget = robot.getArmAngle(), armExtendTarget = robot.getArmExtension(); // Initialize arm to current position
+//        double gripper   = 0;
+        double aLastTime = 0, bLastTime = 0, xLastTime = 0, yLastTime = 0, rBLastTime = 0, lBLastTime = 0;
+        boolean backButtonPressed = false;
+        final double BUTTON_PRESS_DELAY = .075;// seconds, keep track of how long a button has been pressed and allow for a quick press to move a servo a small amount while a long press moves the servo a longer distance.
+
+        // initialize all the hardware, using the hardware class. See how clean and simple this is?
+        robot.init();
+
+        // Send telemetry message to signify robot waiting;
+        telemetry.addData(">", "Robot Ready.  Press Play to start OpMode.");    //
+        telemetry.update();
+
+        // Wait for the game to start (driver presses PLAY)
+        waitForStart();
+
+        // run until the end of the match (driver presses STOP)
+        while (opModeIsActive()) {
+
+            // Save CPU resources; can resume streaming when needed.
+            // This uses a method of tracking the state of the button called latching. See https://stemrobotics.cs.pdx.edu/node/7262.html.
+            // It won't toggle until the button is released. This avoids double-presses.
+            if (gamepad1.back) {
+                backButtonPressed = true;
+            }
+            else if (backButtonPressed && !gamepad1.back)
+            {
+                backButtonPressed = false;
+//                robot.toggleStreaming();
+            }
+                
+
+            // Run wheels in POV mode (note: The joystick goes negative when pushed forward, so negate it)
+            // In this mode the Left stick moves the robot fwd and back, the Right stick turns left and right.
+            // This way it's also easy to just drive straight, or just turn.
+
+            ////Mr. Morris: Alternatively we could use right trigger for forward, left trigger for reverse, left_stick_x for turning
+            drive = -gamepad1.left_stick_y;
+            turn  =  gamepad1.right_stick_x;
+
+            // Combine drive and turn for blended motion. Use org.firstinspires.ftc.teamcode.RobotHardware class
+            robot.driveRobot(drive, turn);
+
+            // Use gamepad left & right Bumpers to open and close the gripper claw
+            // Use the SERVO constants defined in org.firstinspires.ftc.teamcode.RobotHardware class.
+            // Each time around the loop, the servos will move by a small amount.
+            // Limit the total offset to half of the full travel range
+
+            // Open gripper when right bumper is pressed if it's not already at max, close gripper when left bumper is pressed if it's not already at min
+            // Keeps track of how long a button is pressed and moves a small amount for a short press and a larger amount for a long press
+//            if (gamepad1.right_bumper)
+//                if (getRuntime() - rBLastTime > BUTTON_PRESS_DELAY)
+//                {
+//                    if (gripper < RobotHardware.GRIPPER_MAX)
+//                        gripper += RobotHardware.GRIPPER_SPEED;
+//                    rBLastTime = getRuntime();
+//                }
+//            else if (gamepad1.left_bumper)
+//                {
+//                    if (getRuntime() - lBLastTime > BUTTON_PRESS_DELAY)
+//                        if (gripper > RobotHardware.GRIPPER_MIN)
+//                            gripper -= RobotHardware.GRIPPER_INCREMENT;
+//                    lBLastTime = getRuntime();
+//                }
+//            gripper = Range.clip(gripper, -0.5, 0.5);
+//
+//            // Move servo to new position.  Use org.firstinspires.ftc.teamcode.RobotHardware class
+//            robot.setGripperPosition(gripper);
+
+            // Use gamepad buttons to move arm up (Y) and down (A)
+            // Use the MOTOR constants defined in org.firstinspires.ftc.teamcode.RobotHardware class.
+            //// Mr. Morris: Consider redefining the arm movements to use a joystick with a,b,x,y buttons reserved for preset positions like in FTC season
+//            if (gamepad1.y)
+//                if (getRuntime() - yLastTime > BUTTON_PRESS_DELAY) {
+//                    if (armRotateTarget < RobotHardware.ARM_ROTATE_MAX)
+//                        armRotateTarget += RobotHardware.ARM_INCREMENT_DEGREES; // rotate arm up when Y is pressed
+//                    yLastTime = getRuntime();
+//                }
+//            else if (gamepad1.a)
+//                if (getRuntime() - aLastTime > BUTTON_PRESS_DELAY) {
+//                    if (armRotateTarget > RobotHardware.ARM_ROTATE_MIN)
+//                        armRotateTarget -= RobotHardware.ARM_INCREMENT_DEGREES; // rotate arm down when A is pressed
+//                    aLastTime = getRuntime();
+//                }
+
+            // Use gamepad buttons to extend arm (X) and retract arm (B)
+            // Use the MOTOR constants defined in org.firstinspires.ftc.teamcode.RobotHardware class.
+//            if (gamepad1.x && getRuntime() - xLastTime > BUTTON_PRESS_DELAY) {
+//                if (armExtendTarget < RobotHardware.ARM_EXTEND_MAX)
+//                    armExtendTarget += RobotHardware.ARM_EXTEND_POWER; // extend when X is pressed
+//                xLastTime = getRuntime();
+//                }
+//            else if (gamepad1.b && getRuntime() - bLastTime > BUTTON_PRESS_DELAY) {
+//                if (armExtendTarget > RobotHardware.ARM_RETRACT_MAX) {
+//                    armExtendTarget -= RobotHardware.ARM_RETRACT_POWER; // retract when B is pressed
+//                    // CLEAN UP LANGUAGE - refers to power but should be angle, make it like arm rotate
+//                }
+//                bLastTime = getRuntime();
+//            }
+//            else armExtendTarget = 0; // don't move if x and b aren't pressed.
+//            robot.setArmPosition(armRotateTarget, armExtendTarget); // send rotation and extension values to robot
+//
+//            // Move wrist so that it moves when arm rotates to keep gripper parallel to floor
+//            // e.g. if arm angle is at -30 (30 degrees below forward horizontal), wrist must be 30 (30 degrees above forward horizontal) to keep gripper horizontal
+//            robot.setWristAngle(-robot.getArmAngle());
+
+            // Send telemetry messages to explain controls and show robot status
+            telemetry.addData("Drive", "Left Stick");
+            telemetry.addData("Turn", "Right Stick");
+            telemetry.addData("Arm Up/Down", "Y & A Buttons");
+            telemetry.addData("Arm Extend/Retract", "X & B Buttons");
+            telemetry.addData("Gripper Open/Closed", "Left and Right Bumpers");
+            telemetry.addData("-", "-------");
+
+            telemetry.addData("Drive Power", "%.2f", drive);
+            telemetry.addData("Turn Power",  "%.2f", turn);
+//            telemetry.addData("Arm Rotate Power",  "%.2f", armRotateTarget);
+//            telemetry.addData("Arm Extend Power",  "%.2f", armExtendTarget);
+//            telemetry.addData("Gripper Position",  "Offset = %.2f", gripper);
+            telemetry.update();
+
+            idle(); //share processor with other programs - good to include in any loop structure in a linear OpMode
+        }
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotHardware.java
@@ -0,0 +1,271 @@
+package org.firstinspires.ftc.teamcode;
+/* Copyright (c) 2022 FIRST. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted (subject to the limitations in the disclaimer below) provided that
+ * the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this list
+ * of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice, this
+ * list of conditions and the following disclaimer in the documentation and/or
+ * other materials provided with the distribution.
+ *
+ * Neither the name of FIRST nor the names of its contributors may be used to endorse or
+ * promote products derived from this software without specific prior written permission.
+ *
+ * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
+ * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.hardwareMap;
+
+import android.util.Size;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.Range;
+
+import org.firstinspires.ftc.robotcore.external.hardware.camera.BuiltinCameraDirection;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.vision.VisionPortal;
+
+/*
+ * This file works in conjunction with the External Hardware Class sample called: org.firstinspires.ftc.teamcode.MorrisPOVDrive.java
+ * Please read the explanations in that Sample about how to use this class definition.
+ *
+ * This file defines a Java Class that performs all the setup and configuration for a sample robot's hardware (motors and sensors).
+ * It assumes six motors (left_front_drive, left_rear_drive, right_front_drive, right_rear_drive, arm_rotate, and arm_extend) and two servos (wrist and gripper)
+ *
+ * This one file/class can be used by ALL of your OpModes without having to cut & paste the code each time.
+ *
+ * Where possible, the actual hardware objects are "abstracted" (or hidden) so the OpMode code just makes calls into the class,
+ * rather than accessing the internal hardware directly. This is why the objects are declared "private".
+ *
+ * Use Android Studio to Copy this Class, and Paste it into your team's code folder with *exactly the same name*.
+ *
+ * Or... In OnBot Java, add a new file named org.firstinspires.ftc.teamcode.RobotHardware.java, select this sample, and select Not an OpMode.
+ * Also add a new OpMode, select the sample org.firstinspires.ftc.teamcode.MorrisPOVDrive.java, and select TeleOp.
+ *
+ */
+
+////Mr. Morris made changes to have 4 drive motors, arm rotation and extension, and wrist and gripper servos
+
+public class RobotHardware {
+
+    /* Declare OpMode members. */
+    private LinearOpMode myOpMode = null;   // gain access to methods in the calling OpMode.
+
+    // Define Motor and Servo objects  (Make them private so they can't be accessed externally)
+    private DcMotor leftFrontDrive   = null;
+    private DcMotor leftRearDrive   = null;
+    private DcMotor rightFrontDrive  = null;
+    private DcMotor rightRearDrive  = null;
+//    private DcMotor armRotate = null;
+//    private DcMotor armExtend = null;
+//    private Servo   wrist = null;
+//    private Servo   gripper = null;
+
+    // Define Drive constants.  Make them public so they CAN be used by the calling OpMode
+    //Mr. Morris: TO DO: test and update servo speeds.
+//    public static final double MID_SERVO       =  0.5 ;
+//    public static final double GRIPPER_INCREMENT = 0.02, GRIPPER_MAX = 1, GRIPPER_MIN = 0 ;  // sets rate to move gripper servo and max and min travel. If you use SRS servo programmer to set limits, this will be 1 and 0. If you need to limit travel in the software, this is where to do it.
+//    public static final double WRIST_SPEED = 0.02 ; // sets rate to move wrist servo
+//    public static final double WRIST_MAX_ANGLE  = 300 ; // Adjust this angle if SRS servo programmer has limited servo travel to less than 300
+//    public static final int ARM_INCREMENT_DEGREES = 5, ARM_ROTATE_MAX = 225, ARM_ROTATE_MIN = -45 ;
+//    public static final double ARM_ROTATE_ENCODER_RESOLUTION = 2786.2, ARM_ROTATE_GEAR_RATIO = 20 ;
+//    public static final double ARM_EXTEND_POWER  = 0.10, ARM_RETRACT_POWER  = -0.10 ;
+//    public static final double ARM_EXTEND_MAX = 1000; // TO DO: This is almost certainly a wrong number for the max travel.
+//    public static final double ARM_RETRACT_MAX = 0; // TO DO: This is almost certainly a wrong number for the max travel.
+
+    // Define vision defaults
+//    private static final boolean USE_WEBCAM = true;  // true for webcam, false for phone camera
+//    private boolean streaming = true; // streaming starts off true, gets toggled in toggleStreaming
+
+    // The variable to store our instance of the vision portal.
+//    private VisionPortal visionPortal;
+
+    // Define a constructor that allows the OpMode to pass a reference to itself.
+    public RobotHardware(LinearOpMode opmode) {
+        myOpMode = opmode;
+    }
+
+    /**
+     * Initialize all the robot's hardware.
+     * This method must be called ONCE when the OpMode is initialized.
+     * <p>
+     * All of the hardware devices are accessed via the hardware map, and initialized.
+     */
+    public void init()    {
+        // Define and Initialize Motors (note: need to use reference to actual OpMode).
+        leftFrontDrive  = myOpMode.hardwareMap.get(DcMotor.class, "left_front_drive");
+        leftRearDrive  = myOpMode.hardwareMap.get(DcMotor.class, "left_rear_drive");
+        rightFrontDrive = myOpMode.hardwareMap.get(DcMotor.class, "right_front_drive");
+        rightRearDrive  = myOpMode.hardwareMap.get(DcMotor.class, "right_rear_drive");
+//        armRotate   = myOpMode.hardwareMap.get(DcMotor.class, "arm_rotate");
+//        armExtend   = myOpMode.hardwareMap.get(DcMotor.class, "arm_extend");
+
+        // To drive forward, most robots need the motor on one side to be reversed, because the axles point in opposite directions.
+        // Pushing the left stick forward MUST make robot go forward. So adjust these two lines based on your first test drive.
+        // Note: The settings here assume a 90 Deg drive on all four wheels. A Gear Reduction or direct drive may require direction flips
+        leftFrontDrive.setDirection(DcMotor.Direction.FORWARD);
+        leftRearDrive.setDirection(DcMotor.Direction.FORWARD);
+        rightFrontDrive.setDirection(DcMotor.Direction.REVERSE);
+        rightRearDrive.setDirection(DcMotor.Direction.REVERSE);
+
+        // Since there are encoders connected, RUN_USING_ENCODER mode is enabled for greater accuracy
+        leftFrontDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        leftRearDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        rightFrontDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        rightRearDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+/*      armRotate.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        armExtend.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        // Define and initialize ALL installed servos.
+        wrist = myOpMode.hardwareMap.get(Servo.class, "wrist");
+        gripper = myOpMode.hardwareMap.get(Servo.class, "gripper");
+        wrist.setPosition(MID_SERVO);
+        gripper.setPosition(MID_SERVO);
+
+        // Create the vision portal by using a builder.
+        VisionPortal.Builder builder = new VisionPortal.Builder();
+
+        // Set the camera (webcam vs. built-in RC phone camera).
+        if (USE_WEBCAM) {
+            builder.setCamera(hardwareMap.get(WebcamName.class, "Webcam 1"));
+        } else {
+            builder.setCamera(BuiltinCameraDirection.BACK);
+        }
+
+        // Choose a camera resolution. Not all cameras support all resolutions.
+        builder.setCameraResolution(new Size(640, 480));
+
+        // Enable the RC preview (LiveView).  Set "false" to omit camera monitoring.
+        builder.enableLiveView(true);
+
+        // Set the stream format; MJPEG uses less bandwidth than default YUY2.
+        builder.setStreamFormat(VisionPortal.StreamFormat.MJPEG);
+
+        // Choose whether or not LiveView stops if no processors are enabled.
+        // If set "true", monitor shows solid orange screen if no processors enabled.
+        // If set "false", monitor shows camera view without annotations.
+        builder.setAutoStopLiveView(false);
+
+        // Build the Vision Portal, using the above settings.
+        visionPortal = builder.build();
+*/
+        myOpMode.telemetry.addData("DS preview on/off", "3 dots, Camera Stream");
+        myOpMode.telemetry.addData(">", "Hardware Initialized");
+        myOpMode.telemetry.update();
+    }
+
+    /**
+     * Calculates the left/right motor powers required to achieve the requested
+     * robot motions: Drive (Axial motion) and Turn (Yaw motion).
+     * Then sends these power levels to the motors.
+     *
+     * @param Drive     Fwd/Rev driving power (-1.0 to 1.0) +ve is forward
+     * @param Turn      Right/Left turning power (-1.0 to 1.0) +ve is CW
+     */
+    public void driveRobot(double Drive, double Turn) {
+        // Combine drive and turn for blended motion.
+        double left  = Drive + Turn;
+        double right = Drive - Turn;
+
+        // Scale the values so neither exceed +/- 1.0
+        double max = Math.max(Math.abs(left), Math.abs(right));
+        if (max > 1.0)
+        {
+            left /= max;
+            right /= max;
+        }
+
+        // Use existing function to drive wheels on both sides..
+        setDrivePower(left, right);
+    }
+
+    /**
+     * Pass the requested wheel motor powers to the appropriate hardware drive motors.
+     *
+     * Mr. Morris: Our robot is 4 wheel drive, but in tank drive configuration the two left wheels
+     *             always travel at the same speed and the two right wheels travel at the same speed.
+     *
+     * @param leftWheel     Fwd/Rev driving power (-1.0 to 1.0) +ve is forward
+     * @param rightWheel    Fwd/Rev driving power (-1.0 to 1.0) +ve is forward
+     */
+    public void setDrivePower(double leftWheel, double rightWheel) {
+        // Output the values to the motor drives.
+        leftFrontDrive.setPower(leftWheel);
+        leftRearDrive.setPower(leftWheel);
+        rightFrontDrive.setPower(rightWheel);
+        rightRearDrive.setPower(rightWheel);
+    }
+
+//    /**
+//     * Pass the requested arm power to the appropriate hardware drive motor
+//     *
+//     * @param targetAngle angle from -45 to 225
+//     * @param targetExtension encoder value ------ Range?
+//     */
+//    public void setArmPosition(double targetAngle, double targetExtension) {
+//        armRotate.setTargetPosition((int) targetAngle);
+//        armExtend.setTargetPosition((int) targetExtension);
+//        /**
+//         * Need to write code to set power whose direction is dependent on current position,
+//         * preferably a PID type implementation
+//         */
+//    }
+//
+//    /**
+//     * Get the encoder information for the arm rotation motor and convert it to degrees.
+//     * Mr. Morris: TO DO: Might need to initialize arm angle on startup and/or adjust for starting/resting position.
+//     *                    i.e. if starting location is -45 degrees, initialize and offset it at program start to account for this.
+//     *                    Add telemetry statement to test and adjust this.
+//     */
+//    public double getArmAngle(){
+//        return armRotate.getCurrentPosition() * 360 / (ARM_ROTATE_ENCODER_RESOLUTION * ARM_ROTATE_GEAR_RATIO);
+//    }
+//
+//    public double getArmExtension(){
+//        return armExtend.getCurrentPosition();
+//    }
+//
+//    /**
+//     * Mr. Morris: TO DO: may want to work in degrees, then convert to range from -0.5 to 0.5, see setWristAngle() function
+//     * Send the gripper the new position to go to
+//     * @param offset distance from center mid point.
+//     */
+//    public void setGripperPosition(double offset) {
+//        offset = Range.clip(offset, -0.5, 0.5);
+//        gripper.setPosition(MID_SERVO + offset);
+//    }
+//
+//    /**
+//     * Send the wrist to a certain angle in degrees
+//     * @param angle is the angle the wrist should go to, in degrees
+//     */
+//    public void setWristAngle(double angle){
+//        angle = Range.clip(angle / WRIST_MAX_ANGLE, -0.5, 0.5); // convert angle in degrees to a range from -0.5 to 0.5.
+//        wrist.setPosition(MID_SERVO + angle);
+//    }
+//
+//    // Toggle streaming on/off to save CPU resources
+//    public void toggleStreaming(){
+//        if (streaming) visionPortal.stopStreaming();
+//        else {
+//            visionPortal.resumeStreaming();
+//        }
+//        streaming = !streaming;
+//    }
+}
